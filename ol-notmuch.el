06288ed5ec088f2702afb8f0d952f7db18bb7d56
Jonas Bernoulli <jonas@bernoul.li>
2025-06-01 00:28:19 +0200

    Release version 2.1.1

--- ol-notmuch.el
+++ ol-notmuch.el
@@ -9,12 +9,12 @@
 ;; Homepage: https://github.com/tarsius/ol-notmuch
 ;; Keywords: hypermedia mail
 
-;; Package-Version: 2.1.0
+;; Package-Version: 2.1.1
 ;; Package-Requires: (
 ;;     (emacs "29.1")
-;;     (compat "30.0.2.0")
+;;     (compat "30.1")
 ;;     (notmuch "0.38.2")
-;;     (org "9.7.19"))
+;;     (org "9.7"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
